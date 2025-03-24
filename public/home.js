ce7e2021b34b201b217de1fe6764edc90fa52dcf
Barnabas James <barnabasjames27@gmail.com>
2025-03-24 13:16:39 +0100

    Fixed swiper js to work well and breakpoint is now functional

--- public/home.js
+++ public/home.js
@@ -1,5 +1,6 @@
 const swiper = new Swiper('.swiper-wrapper', {
     loop: true,
+    spaceInBetween: true
   
     // If we need pagination
     pagination: {
@@ -11,4 +12,23 @@ const swiper = new Swiper('.swiper-wrapper', {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
+
+    //break points 
+    breakpoints: {
+        // when window width is >= 320px
+        375: {
+          slidesPerView: 1,
+          spaceBetween: 10
+        },
+        // when window width is >= 480px
+        650: {
+          slidesPerView: 2,
+          spaceBetween: 20
+        },
+        // when window width is >= 640px
+        1020: {
+          slidesPerView: 3,
+          spaceBetween: 30
+        }
+    }
 });
